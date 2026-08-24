1fa07d6b298b1c2f39782eff3100a5ef46b23248
Егор Мостренов <zeniskyy@gmail.com>
2026-08-24 20:18:59 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,7 @@ local HttpGet = game.HttpGet
 local GameId: number = game.GameId
 
 local Games: {[number]: string} = loadstring(
-  HttpGet(game, "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua")
+  HttpGet(game, "https://raw.githubusercontent.com/ReversalReside/ReversalHub/refs/heads/main/GameList.lua")
 )() :: any
 
 local URL: string? = Games[GameId]
