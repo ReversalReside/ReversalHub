4ae569783ba85fb7d3be0752db1e60e83d4ff9ce
Егор Мостренов <zeniskyy@gmail.com>
2026-09-14 22:25:15 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
   [994732206] = "https://raw.githubusercontent.com/ReversalReside/ReversalHub/refs/heads/main/Games/bloxfruits.lua", -- Blox Fruit
+  [6760085372] = "https://raw.githubusercontent.com/ReversalReside/ReversalHub/refs/heads/main/Games/JujutsuZero.lua", -- Jujutsu Zero
 }
 
 return Games
